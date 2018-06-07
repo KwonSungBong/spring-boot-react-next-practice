ca0e568cc555869cf6881d054823e0e39b11e397
ksb <ksb>
2018-06-07 09:36:15 +0900

    teset

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react'
 import Link from 'next/link'
 import withRedux from 'next-redux-wrapper'
 
-import Main from '../components'
-
 import { initStore } from '../store'
 
 class Index extends Component {
   render () {
     return <div>
         <div>
+            <Link href='/form' as='/form'><a>form</a></Link>
+        </div>
+        <div>
             <Link href='/async/async' as='/async/async'><a>asyncasync</a></Link>
         </div>
         <div>
             <Link href='/async' as='/async'><a>post</a></Link>
         </div>
-      <Main />
     </div>
   }
 }
